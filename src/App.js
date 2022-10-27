a20f2b4138d11ef489d78599271bcf7efd10c5f5
luis campero <admin@paisa.mx>
2022-10-27 13:18:45 -0500

    Camperocks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Camperocks!!!!
+          Edit <code>src/App.js</code> and save to reload. Camperocks se rifa!!!!
         </p>
         <a
           className="App-link"
